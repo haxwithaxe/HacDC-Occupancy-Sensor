e96d5d20e2a88c211185c63e67343895e0ee047a
Chris Koepke <me@haxwithaxe.net>
2012-06-06 16:50:10 -0400

    shortcircuiting the socket communication with plain old checking

--- botutil.py
+++ botutil.py
@@ -2,6 +2,7 @@ DEBUG = True
 
 import json
 import threading
+import time
 from util import *
 from config import config as configmod
 
@@ -26,14 +27,19 @@ class _update_on_change(threading.Thread):
 		threading.Thread.__init__(self)
 
 	def run(self):
+		last_status = None
 		while not self.die:
-			msg = self.sock.msg[:]
-			self.sock.clear_msg()
-			if config['update_flag'] in [x.strip() for x in msg]:
+			status = unstash('dict')
+			if last_status and last_status != status:
 				self.bot.update()
-			for m in msg:
-				if m.strip().startswith('pass:'):
-					bot.pass_msg(m.replace('pass:',1).strip())
+			#msg = self.sock.msg[:]
+			#self.sock.clear_msg()
+			#if config['update_flag'] in [x.strip() for x in msg]:
+			#	self.bot.update()
+			#for m in msg:
+			#	if m.strip().startswith('pass:'):
+			#		bot.pass_msg(m.replace('pass:',1).strip())
+			time.sleep(0.1)
 
 	def die(self):
 		self.die = True
