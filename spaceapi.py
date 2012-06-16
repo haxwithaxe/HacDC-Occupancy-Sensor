160d4df2a26ea2c11fe807aa891498da80eac567
occsensor <occsensor@localhost>
2012-06-16 14:15:46 -0400

    spaceapi.encode takes status.cache and returns a valid spaceapi v0.12 json string

--- spaceapi.py
+++ spaceapi.py
@@ -7,9 +7,20 @@ conf = config().config
 debug = Debug()
 
 def encode(status):
-	apidict = file2json(conf['space_api_tmpl'])
+	apidict = file2json(conf['space_api_tmpl_json'])
 	apidict.update({'open':status['status']})
 	apidict.update(file2json(conf['hacdc_spaceapi_json']))
-	apidict['sensors']['light-switch'].update(json.loads(status['raw']))
-	return apidict
+	rs = json.loads(status['raw'])
+	apidict['sensors'] = [
+				{'light-switch':
+					{'main_room':rs['main_light'],'work_room':rs['work_light'],'hall':rs['hall_light']}
+				},
+				{'motion':
+					{'main_room':rs['main_pir'],'work_room':rs['work_pir'],'hall':rs['hall_pir']}
+				}
+			]
+	return json.dumps(apidict)
 
+if __name__ == '__main__':
+	status = unstash('dict')
+	print(encode(status))
